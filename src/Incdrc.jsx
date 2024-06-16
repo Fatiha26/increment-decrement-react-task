304a9ffd0e48bb233295e7a6a9a0f5801ac9cb71
Lamia Fatiha <f6lamia@gmail.com>
2024-06-16 12:07:41 +0600

    task done

--- src/Incdrc.jsx
+++ src/Incdrc.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Incdrc = () => {
+
+    const [num, setNum] = useState(0);
+
+    const incnum = () => {
+        setNum(num+1);
+    }
+
+    const drcnum = () => {
+        if(num >0)
+            {
+                setNum(num-1);
+            }
+        else{
+            alert("Sorry! Zero limit reached!")
+            setNum(0);
+        }
+    }
+
+
     return (
         <>
             <div className='main_div'>
                 <div className="center_div">
-                    <h1>0</h1>
+                    <h1>{num}</h1>
                     <div className="btn_div">
-                        <button>Increment</button>
-                        <button>Decrement</button>
+                        <button onClick={incnum}>Increment</button>
+                        <button onClick={drcnum}>Decrement</button>
                     </div>
                 </div>
 
